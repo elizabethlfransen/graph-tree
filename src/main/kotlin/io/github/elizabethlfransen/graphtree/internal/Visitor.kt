29764ead97f450a6910355c728d6d9c904c6d573
Liz Fransen <elizabethlfransen@gmail.com>
2022-09-11 20:23:16 -0500

    Update visitor

--- src/main/kotlin/io/github/elizabethlfransen/graphtree/internal/Visitor.kt
+++ src/main/kotlin/io/github/elizabethlfransen/graphtree/internal/Visitor.kt
@@ -1,4 +1,6 @@
 package io.github.elizabethlfransen.graphtree.internal
 
-interface Visitor<T> {
+internal sealed interface Visitor<T> {
+    fun visit(node: LabelNode): T
+    fun visit(node: DocumentNode): T
 }
